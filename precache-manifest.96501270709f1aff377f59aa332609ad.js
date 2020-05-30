c0bc9ab46c8d006c3d6833e5dd090decbd807300
Devan Hurst <devanh@nulogy.com>
2020-05-30 18:26:03 -0400

    Updates

---
+++ precache-manifest.96501270709f1aff377f59aa332609ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404abea636faba485dcb27386bb3a8fd",
+    "url": "/puzzleboard/index.html"
+  },
+  {
+    "revision": "d7ec15971e23231485a5",
+    "url": "/puzzleboard/static/js/2.3756bf2a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/puzzleboard/static/js/2.3756bf2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201f8499dcb10fb0cf33",
+    "url": "/puzzleboard/static/js/main.3b24851d.chunk.js"
+  },
+  {
+    "revision": "3d98e1bf9e24054dbcfe",
+    "url": "/puzzleboard/static/js/runtime-main.2cd87651.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0da3efc49c17d571d5f3cf611a1006b4.js"
+  "/puzzleboard/precache-manifest.96501270709f1aff377f59aa332609ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/puzzleboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
