32f9be2ef3b5e9ddc90d685ba7f183a0f51e0f10
Devan Hurst <devanh@nulogy.com>
2020-05-30 00:03:17 -0400

    Updates

---
+++ precache-manifest.86ac1d60251f9296f0c852d66a38516b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c968757d59eee2e44b41bb587e9fbe19",
+    "url": "/puzzleboard/index.html"
+  },
+  {
+    "revision": "f03bf312049bc3edb2ce",
+    "url": "/puzzleboard/static/js/2.7a924fed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/puzzleboard/static/js/2.7a924fed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "654f3bc6cc97ce0cb106",
+    "url": "/puzzleboard/static/js/main.d72137a0.chunk.js"
+  },
+  {
+    "revision": "3d98e1bf9e24054dbcfe",
+    "url": "/puzzleboard/static/js/runtime-main.2cd87651.js"
+  }
+]);
